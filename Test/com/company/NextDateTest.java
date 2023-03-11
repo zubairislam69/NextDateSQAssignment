391cd9fa514d667985dedc67479423b4a759c744
zubxxr <zubxxr@gmail.com>
2023-03-10 20:04:44 -0500

    Final changes

--- Test/com/company/NextDateTest.java
+++ Test/com/company/NextDateTest.java
@@ -11,27 +11,38 @@ public class NextDateTest {
         assertEquals(NextDate.NextDate(31, 12, 2212), "Invalid date");
         assertEquals(NextDate.NextDate(31, 4, 1913), "Invalid date");
         assertEquals(NextDate.NextDate(32, 5, 1999), "Invalid date");
+        assertEquals(NextDate.NextDate(0, 5, 1999), "Invalid date");
+
     }
 
     @Test
-    public void outOfRange() {
+    public void invalidMonth() {
+        assertEquals(NextDate.NextDate(4, 20, 1999), "Invalid date");
+        assertEquals(NextDate.NextDate(10, 13, 2024), "Invalid date");
+        assertEquals(NextDate.NextDate(10, 0, 2024), "Invalid date");
+    }
+
+    @Test
+    public void invalidYear() {
         assertEquals(NextDate.NextDate(4, 7, 1811), "Cannot process dates before 1812 and after 2212");
+        assertEquals(NextDate.NextDate(4, 7, 3000), "Cannot process dates before 1812 and after 2212");
+
     }
 
     @Test
-    public void leapYear () {
+    public void leapYear() {
         assertEquals(NextDate.NextDate(28, 2, 1812), "29 2 1812");
         assertEquals(NextDate.NextDate(29, 2, 1816), "1 3 1816");
     }
 
     @Test
-    public void nonLeapYear () {
+    public void nonLeapYear() {
         assertEquals(NextDate.NextDate(28, 2, 2011), "1 3 2011");
         assertEquals(NextDate.NextDate(29, 2, 2011), "Invalid date");
     }
 
     @Test
-    public void nextDay () {
+    public void nextDay() {
         assertEquals(NextDate.NextDate(1, 12, 2212), "2 12 2212");
         assertEquals(NextDate.NextDate(30, 4, 1913), "1 5 1913");
         assertEquals(NextDate.NextDate(31, 12, 2022), "1 1 2023");
@@ -39,8 +50,9 @@ public class NextDateTest {
     }
 
     @Test
-    public void nextYear () {
+    public void nextYear() {
         assertEquals(NextDate.NextDate(31, 12, 2022), "1 1 2023");
+        assertEquals(NextDate.NextDate(31, 12, 1900), "1 1 1901");
     }
 
 }
--- src/com/company/NextDate.java
+++ src/com/company/NextDate.java
@@ -1,16 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class NextDate {
     public static void main(String[] args) {
 
-        NextDate(30,12,2212);
+        Scanner dateInput = new Scanner(System.in);
+
+        int day = dateInput.nextInt();
+        int month = dateInput.nextInt();
+        int year = dateInput.nextInt();
+
+        System.out.println("Enter any date to get the next day. (Only valid years: 1812 - 2212)");
+        System.out.println("Please enter in format dd mm yy: ");
+
+        System.out.println("The next date is: " + NextDate(day, month, year));
     }
 
     public static String NextDate(int day, int month, int year) {
-
-        int receivedDay = day;
-        int receivedMonth = month;
-        int receivedYear = year;
+        int newDay = day;
+        int newMonth = month;
+        int newYear = year;
 
         boolean error = false;
         boolean isLeapYear = false;
@@ -18,14 +28,15 @@ public class NextDate {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
             isLeapYear = true;
 
-
-
-        // if date given is 30 and month is ___________
+        // if day given is 30 and month is one of the months that have 30 days
+        // set next date as the first of the next month
         if (day == 30 && (month == 4 || month == 6 || month == 9 || month == 11)) {
-            receivedDay = 1;
-            receivedMonth += 1;
+            newDay = 1;
+            newMonth += 1;
         }
 
+        // if day given is 31 and month is one of the months that have 30 days
+        // then return error
         else if (day == 31 && (month == 4 || month == 6 || month == 9 || month == 11)) {
             error = true;
             System.out.println("Invalid date");
@@ -33,75 +44,96 @@ public class NextDate {
         }
 
 
-        // if date given is 31 and month is ___________
+        // if day given is 31 and month is one of the months that have 31 days
+        // set next date as the first of the next month
         else if (day == 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8
                 || month == 10)) {
-            receivedDay = 1;
-            receivedMonth += 1;
+            newDay = 1;
+            newMonth += 1;
         }
 
-        // if date given is 28 and month is february and it is a leap year
+        // if day given is 28, month is february, and it is a leap year
+        // increment the day to 29
         else if (day == 28 && month == 2 && isLeapYear )
-            receivedDay += 1;
+            newDay += 1;
 
 
-            // if date given is 28 and month is february and it is not a leap year
+        // if day given is 28 and month is february, but it is not a leap year
+        // set next date as the first of the next month
         else if (day == 28 && month == 2 && !isLeapYear ) {
-            receivedDay = 1;
-            receivedMonth += 1;
+            newDay = 1;
+            newMonth += 1;
         }
 
-        // if date given is between 29 and month is february, and it is leap year
+        // if day given is 29, month is february, and it is leap year
+        // set next date as the first of the next month
         else if (day == 29 && month == 2 && isLeapYear) {
-            receivedDay = 1;
-            receivedMonth += 1;
+            newDay = 1;
+            newMonth += 1;
         }
 
-        else if (year < 1812 || year > 2212) {
-            error = true;
-            System.out.println("Cannot process dates before 1812 and after 2212");
-            return "Cannot process dates before 1812 and after 2212";
-        }
-
-        // if date given is between 29 and month is february, and it is not a leap year
+        // if day is 29, month is february, and it is not a leap year
+        //print error
         else if (day == 29 && month == 2 && !isLeapYear) {
             error = true;
             System.out.println("Invalid date");
             return "Invalid date";
         }
 
+        //if an invalid year is inputted, print error
+        else if (year < 1812 || year > 2212) {
+            error = true;
+            System.out.println("Cannot process dates before 1812 and after 2212");
+            return "Cannot process dates before 1812 and after 2212";
+        }
+
+        //if month is february but day is 30 or 31
+        //then print error
         else if ((day == 30 || day == 31) && month == 2) {
             error = true;
             System.out.println("Invalid date");
             return "Invalid date";
         }
 
+        //if month is december and day is 31 but the next year is out of bounds(2213),
+        //then print error
         else if (month == 12 && day == 31 && (year + 1 == 2213)) {
             error = true;
             System.out.println("Invalid date");
             return "Invalid date";
         }
 
+        //if month is december and day is 31, set date as first day of first month of next year
         else if (month == 12 && day == 31) {
-            receivedYear += 1;
-            receivedDay = 1;
-            receivedMonth = 1;
+            newYear += 1;
+            newDay = 1;
+            newMonth = 1;
         }
 
-        else if (day >= 32) {
+        //if day is greater than 31 or day is less than 1, print error
+        else if (day > 31 || day < 1) {
             error = true;
             System.out.println("Invalid date");
             return "Invalid date";
 
         }
 
+        //if month given is greater than 12 or less than 1, print error
+        else if (month > 12 || month < 1) {
+            error = true;
+            System.out.println("Invalid date");
+            return "Invalid date";
+        }
+
+        // increment date to the next day
         else
-            receivedDay += 1;
+            newDay += 1;
 
 
+        //if error is false, print out date of next day
         if (!error)
-            System.out.println(receivedDay + " " + receivedMonth + " " + receivedYear);
+            System.out.println(newDay + " " + newMonth + " " + newYear);
 
-        return (receivedDay + " " + receivedMonth + " " + receivedYear);
+        return (newDay + " " + newMonth + " " + newYear);
     }
 }
